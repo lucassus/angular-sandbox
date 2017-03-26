504857db55e78270650f06e09809cb983c5f526a
Lukasz Bandzarewicz <lucassus@gmail.com>
2017-03-26 17:47:05 +0200

    Initial commit

---
+++ e2e/app.e2e-spec.ts
@@ -0,0 +1,16 @@
+import { AngularSandboxPage } from './app.po';
+
+describe('angular-sandbox App', () => {
+
+  let page: AngularSandboxPage;
+
+  beforeEach(() => {
+    page = new AngularSandboxPage();
+  });
+
+  it('should display message saying app works', () => {
+    page.navigateTo();
+    expect(page.getParagraphText()).toEqual('Hello World!');
+  });
+
+});
