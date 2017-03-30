0026c3bec4a98ec68ae40f85dd0cc539c93cbd7e
Lukasz Bandzarewicz <lucassus@gmail.com>
2017-03-30 13:19:23 +0200

    Bump app dependencies

--- client/app/navigation/navigation.component.spec.ts
+++ client/app/navigation/navigation.component.spec.ts
@@ -1,3 +1,4 @@
+import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { NavigationComponent } from './navigation.component';
@@ -9,7 +10,8 @@ describe('NavigationComponent', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [ NavigationComponent ]
+      declarations: [NavigationComponent],
+      schemas: [NO_ERRORS_SCHEMA]
     });
 
     fixture = TestBed.createComponent(NavigationComponent);
