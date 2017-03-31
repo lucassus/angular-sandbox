25f9b23475d4c3f842ee7515fac8a3781a6f6c2e
Lukasz Bandzarewicz <lucassus@gmail.com>
2017-03-31 18:03:18 +0200

    Write specs for appCreateContact component

--- client/app/contacts/create/create.component.spec.ts
+++ client/app/contacts/create/create.component.spec.ts
@@ -2,9 +2,19 @@ import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { ReactiveFormsModule } from '@angular/forms';
 import { Router } from '@angular/router';
+import { stub } from 'sinon';
 
+import { Contact } from '../contact';
 import { ContactsService } from '../contacts.service';
 import { CreateComponent } from './create.component';
+
+const fakeRouter = {
+  navigate: stub()
+};
+
+const fakeContactService = {
+  create: stub().returns(Promise.resolve(new Contact({ id: 124 })))
+};
 
 describe('CreateComponent', () => {
 
@@ -20,21 +30,42 @@ describe('CreateComponent', () => {
         CreateComponent
       ],
       providers: [
-        { provide: Router, useValue: { } },
-        { provide: ContactsService, useValue: {} }
+        { provide: Router, useValue: fakeRouter },
+        { provide: ContactsService, useValue: fakeContactService }
       ],
       schemas: [NO_ERRORS_SCHEMA]
     });
-  });
 
-  beforeEach(() => {
     fixture = TestBed.createComponent(CreateComponent);
     component = fixture.componentInstance;
+
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  describe('.createContact', () => {
+
+    it('creates a contact', () => {
+      component.createContact({ firstName: 'Luke', lastName: 'Skywalker' });
+      expect(fakeContactService.create.called).toBeTruthy();
+
+      const [data] = fakeContactService.create.lastCall.args;
+      expect(data.firstName).toEqual('Luke');
+      expect(data.lastName).toEqual('Skywalker');
+    });
+
+    describe('on success', () => {
+
+      it('redirects to the show page', () => {
+        component.createContact({});
+        expect(fakeRouter.navigate.called).toBeTruthy();
+
+        const [commands] = fakeRouter.navigate.lastCall.args;
+        expect(commands[0]).toEqual('./contacts');
+        expect(commands[1]).toEqual(124);
+      });
+
+    });
+
   });
 
 });
