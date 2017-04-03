e0e05b77c6f1404c14ec5c1ce0b5ccfc504c1d37
Lukasz Bandzarewicz <lucassus@gmail.com>
2017-04-03 14:01:03 +0200

    Add contact country select

--- client/app/contacts/contact-form/contact-form.component.ts
+++ client/app/contacts/contact-form/contact-form.component.ts
@@ -1,7 +1,9 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 
+import { Config } from '../../config';
 import { phoneValidator } from '../../phone-validator';
+import { ICountry } from '../address';
 import { Contact } from '../contact';
 
 @Component({
@@ -12,6 +14,8 @@ export class ContactFormComponent implements OnInit {
 
   @Input('contact') contact: Contact;
   @Output() onSubmit = new EventEmitter();
+
+  countries: Array<ICountry>;
 
   contactForm = new FormGroup({
     firstName: new FormControl('', Validators.required),
@@ -26,9 +30,14 @@ export class ContactFormComponent implements OnInit {
     address: new FormGroup({
       street: new FormControl(),
       town: new FormControl(),
-      zipCode: new FormControl('', Validators.pattern(/^\d{2}-\d{3}$/))
+      zipCode: new FormControl('', Validators.pattern(/^\d{2}-\d{3}$/)),
+      countryCode: new FormControl()
     })
   });
+
+  constructor(config: Config) {
+    this.countries = config.countries;
+  }
 
   ngOnInit(): void {
     const value = this.contact.toJS();
