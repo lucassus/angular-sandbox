89a8fd045f88f786c255b4f27ce459425948ff29
Lukasz Bandzarewicz <lucassus@gmail.com>
2017-04-13 21:04:50 +0200

    Show / hide transcluded content

--- client/app/sandbox/my-component/my-component.component.ts
+++ client/app/sandbox/my-component/my-component.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, ContentChild, forwardRef } from '@angular/core';
 
 @Component({
   selector: 'app-my-component',
   templateUrl: './my-component.component.html'
 })
 export class MyComponentComponent {
+
+  @ContentChild(forwardRef(() => MyComponentContentComponent)) content: MyComponentContentComponent;
+
+  hasContent(): boolean {
+    return this.content !== undefined;
+  }
 
 }
 
