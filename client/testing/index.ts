24545de61a2af0108a87e2aa33e314949437f2a6
Lukasz Bandzarewicz <lucassus@gmail.com>
2017-04-13 10:52:00 +0200

    Add some testing helpers

---
+++ client/testing/index.ts
@@ -0,0 +1,22 @@
+import { DebugElement } from '@angular/core';
+
+// See https://developer.mozilla.org/en-US/docs/Web/API/MouseEvent/button
+/** Button events to pass to `DebugElement.triggerEventHandler` for RouterLink event handler */
+export const ButtonClickEvents = {
+ left:  { button: 0 },
+ right: { button: 2 }
+};
+
+/**
+ * Create custom DOM event the old fashioned way
+ *
+ * https://developer.mozilla.org/en-US/docs/Web/API/Event/initEvent
+ * Although officially deprecated, some browsers (phantom) don't accept the preferred "new Event(eventName)"
+ */
+export function click(el: DebugElement | HTMLElement, eventObj: any = ButtonClickEvents.left): void {
+  if (el instanceof HTMLElement) {
+    el.click();
+  } else {
+    el.triggerEventHandler('click', eventObj);
+  }
+}
