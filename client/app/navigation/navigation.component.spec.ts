adc63ace29ecf2c0be9bca43a3eb488f610dfd15
Lukasz Bandzarewicz <lucassus@gmail.com>
2017-04-14 17:08:32 +0200

    Write more elegant specs for navigation links

--- client/app/navigation/navigation.component.spec.ts
+++ client/app/navigation/navigation.component.spec.ts
@@ -1,4 +1,4 @@
-import { DebugElement, NO_ERRORS_SCHEMA } from '@angular/core';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
 
@@ -10,9 +10,6 @@ describe('NavigationComponent', () => {
 
   let component: NavigationComponent;
   let fixture: ComponentFixture<NavigationComponent>;
-
-  let linkDes: DebugElement[];
-  let links: FakeRouterLinkDirective[];
 
   beforeEach(() => {
     TestBed.configureTestingModule({
@@ -26,34 +23,45 @@ describe('NavigationComponent', () => {
     fixture = TestBed.createComponent(NavigationComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
-
-    // TODO refactor this part
-    linkDes = fixture.debugElement
-      .queryAll(By.directive(FakeRouterLinkDirective));
-
-    links = linkDes.map((de) => {
-      return de.injector.get(FakeRouterLinkDirective);
-    });
   });
 
-  it('has navigation links', () => {
-    expect(links.length).toEqual(4);
+  function findLinkDes() {
+    return fixture.debugElement
+      .queryAll(By.directive(FakeRouterLinkDirective));
+  }
 
-    expect(links[0].linkParams).toEqual('/');
-    expect(links[1].linkParams).toEqual('/');
-    expect(links[2].linkParams).toEqual('/contacts');
-    expect(links[3].linkParams).toEqual('/sandbox');
+  function findLinkFor(path) {
+    return findLinkDes()
+      .map((debugElement) => {
+        const componentInstance = debugElement
+          .injector.get(FakeRouterLinkDirective);
+
+        return { debugElement, componentInstance };
+      })
+      .find(({ componentInstance }) => {
+        return componentInstance.linkParams === path;
+      });
+  }
+
+  it('has navigation links', () => {
+    expect(findLinkDes().length).toEqual(4);
+
+    expect(findLinkFor('/')).not.toBeUndefined();
+    expect(findLinkFor('/contacts')).not.toBeUndefined();
+    expect(findLinkFor('/sandbox')).not.toBeUndefined();
+    expect(findLinkFor('/unknown')).toBeUndefined();
   });
 
   it('can navigate to contacts', () => {
-    // Given
-    const de = linkDes[2];
+    const { debugElement, componentInstance } = findLinkFor('/contacts');
+    click(debugElement);
+    expect(componentInstance.navigatedTo).toEqual('/contacts');
+  });
 
-    // When
-    click(de);
-
-    // Then
-    expect(links[2].navigatedTo).toEqual('/contacts');
+  it('can navigate to sandbox', () => {
+    const { debugElement, componentInstance } = findLinkFor('/sandbox');
+    click(debugElement);
+    expect(componentInstance.navigatedTo).toEqual('/sandbox');
   });
 
 });
