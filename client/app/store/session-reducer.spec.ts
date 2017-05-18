a4492fc2183f9258c482e2158be8dbf4f2ed2652
Lukasz Bandzarewicz <lucassus@gmail.com>
2017-05-18 17:27:46 +0200

    More specs for session reducer

--- client/app/store/session-reducer.spec.ts
+++ client/app/store/session-reducer.spec.ts
@@ -1,5 +1,11 @@
+import {
+  SESSION_AUTHENTICATION_ERROR,
+  SESSION_AUTHENTICATION_SUCCESS,
+  SESSION_CLEAR_AUTHENTICATION_ERROR,
+  SESSION_LOGOUT,
+  SESSION_REQUEST_AUTHENTICATION
+} from './session-actions';
 import { session, SessionState } from './session-reducer';
-import { SESSION_REQUEST_AUTHENTICATION, SESSION_AUTHENTICATION_SUCCESS } from './session-actions';
 
 describe('session', () => {
 
@@ -22,14 +28,17 @@ describe('session', () => {
 
   describe(`${SESSION_REQUEST_AUTHENTICATION} action`, () => {
 
+    let newState: SessionState;
+
+    beforeEach(() => {
+      const state = new SessionState({
+        authenticationPending: false
+      });
+
+      newState = session(state, { type: SESSION_REQUEST_AUTHENTICATION });
+    });
+
     it('toggles `authenticationPending` flag', () => {
-      // Given
-      const state = new SessionState({ authenticationPending: false });
-
-      // When
-      const newState = session(state, { type: SESSION_REQUEST_AUTHENTICATION });
-
-      // Then
       expect(newState.authenticationPending).toBeTruthy();
     });
 
@@ -59,6 +68,70 @@ describe('session', () => {
 
     it('toggles `authenticationError` flag', () => {
       expect(newState.authenticationError).toBeFalsy();
+    });
+
+  });
+
+  describe(`${SESSION_AUTHENTICATION_ERROR} action`, () => {
+
+    let newState: SessionState;
+
+    beforeEach(() => {
+      const state = new SessionState({
+        authenticationPending: true,
+        authenticated: true,
+        authenticationError: false
+      });
+
+      newState = session(state, { type: SESSION_AUTHENTICATION_ERROR });
+    });
+
+    it('toggles `authenticationPending` flag', () => {
+      expect(newState.authenticationPending).toBeFalsy();
+    });
+
+    it('toggles `authenticated` flag', () => {
+      expect(newState.authenticated).toBeFalsy();
+    });
+
+    it('toggles `authenticationError` flag', () => {
+      expect(newState.authenticationError).toBeTruthy();
+    });
+
+  });
+
+  describe(`${SESSION_CLEAR_AUTHENTICATION_ERROR} action`, () => {
+
+    let newState: SessionState;
+
+    beforeEach(() => {
+      const state = new SessionState({
+        authenticationError: true
+      });
+
+      newState = session(state, { type: SESSION_CLEAR_AUTHENTICATION_ERROR });
+    });
+
+    it('toggles `authenticationError` flag', () => {
+      expect(newState.authenticationError).toBeFalsy();
+    });
+
+  });
+
+  describe(`${SESSION_LOGOUT} action`, () => {
+
+    let newState: SessionState;
+
+    beforeEach(() => {
+      const state = new SessionState({
+        authenticated: true
+      });
+
+      newState = session(state, { type: SESSION_LOGOUT });
+    });
+
+    it('toggles `authenticated` flag', () => {
+      expect(newState.authenticated).toBeFalsy();
     });
 
   });
